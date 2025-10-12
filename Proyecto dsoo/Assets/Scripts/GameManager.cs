5d5b9d74bcda85d98db847a3cfcec958e1d5eb83
MateoBenjaminNobaFioriti <mateonoba@isft220.edu.ar>
2025-10-12 19:12:45 -0300

    Se logro generar la grilla donde se van a plantar las plantas

--- Proyecto dsoo/Assets/Scripts/GameManager.cs
+++ Proyecto dsoo/Assets/Scripts/GameManager.cs
@@ -1,11 +1,27 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject Casilla;
+    public int filas = 5;
+    public int columnas = 9;
+    float separacionX = 1.4f;
+    float separacionY = 1.6f;
+    public Vector3 posicionInicial = new Vector3(0,0,0);
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for(int y = 0; y < filas; y++)
+        {
+            for(int x = 0; x < columnas; x++)
+            {
+                Vector3 posicion = posicionInicial + new Vector3(x * separacionX, y * separacionY, 0);
+                Instantiate(Casilla, posicion, quaternion.identity, transform);
+            }
+        }
     }
 
     // Update is called once per frame
