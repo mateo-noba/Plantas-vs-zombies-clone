c40535554b0d48d308d9516bc3a7a5cbf204738d
MateoBenjaminNobaFioriti <mateonoba@isft220.edu.ar>
2025-11-17 20:49:02 -0300

    Continuando con el backend

--- Pagina PVZRogueLike/paginapvzroguelike/src/Models/User.js
+++ Pagina PVZRogueLike/paginapvzroguelike/src/Models/User.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    id: {type: string},
+    nombreDeUsuario: {type: string},
+    email: {type: string},
+    contraseña: {type: string},
+    mejorPuntaje: {type: string},
+    ultimoPuntaje: {type: string}
+});
+
+export default mongoose.model("User", UserSchema);
